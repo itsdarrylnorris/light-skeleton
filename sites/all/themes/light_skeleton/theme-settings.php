85f159e93c97135818fb10c8cc11efabca1228f7
Darryl Norris <admin@darrylnorris.com>
2015-09-19 23:28:21 -0700

    Adding the light_skeleton

---
+++ sites/all/themes/light_skeleton/theme-settings.php
@@ -0,0 +1,6 @@
+<?php
+
+function light_skeleton_form_system_theme_settings_alter(&$form, $form_state) {
+// TODO Considering adding theming configuration.
+// TODO Maybe adding Color Module Support.
+}
---
+++ sites/all/themes/light_skeleton/todo.md
@@ -0,0 +1,5 @@
+## Todo
+
+- We need to add font-awesome
+- We need to provide normice.css cdn.
+- We need to make the menu responsive sticky and working or remove the sticky.
